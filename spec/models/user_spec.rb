e87da84530b08e656438093f9e91337b24eadf56
Evan Blanco <evan.blanco@dotcreek.com>
2018-02-26 13:57:04 -0600

    Test problem

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,22 +1,22 @@
 require 'spec_helper'
 
 describe User do
-  before { @user = FactoryGirl.build(:user) }
+	before { @user = FactoryGirl.build(:user) }
 
-  subject { @user }
+	subject { @user }
 
- 	it { should respond_to(:email) }
- 	it { should respond_to(:password) }
-  	it { should respond_to(:password_confirmation) }
-  	it { should validate_presence_of(:email) }
+	it { should respond_to(:email) }
+	it { should respond_to(:password) }
+	it { should respond_to(:password_confirmation) }
+	it { should validate_presence_of(:email) }
 	it { should validate_uniqueness_of(:email) }
 	it { should validate_confirmation_of(:password) }
 	it { should allow_value('example@domain.com').for(:email) }
 
-  it { should be_valid }
+	it { should be_valid }
 end
 
 describe "when email is not present" do
-  before { @user.email = " " }
-  it { should_not be_valid }
+	before { @user.email = " " }
+	it { should_not be_valid }
 end
