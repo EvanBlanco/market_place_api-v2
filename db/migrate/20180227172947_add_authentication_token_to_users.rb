e8e6c9669f08959e787e221953805d003245061e
Evan Blanco <evan.blanco@dotcreek.com>
2018-02-27 12:03:48 -0600

    Adds sessions controller create action

--- db/migrate/20180227172947_add_authentication_token_to_users.rb
+++ db/migrate/20180227172947_add_authentication_token_to_users.rb
@@ -1,6 +1,13 @@
 class AddAuthenticationTokenToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :auth_token, :string, default: ""
-    add_index :users, :auth_token, unique: true
-  end
+	def change
+		add_column :users, :auth_token, :string, default: ""
+		add_index :users, :auth_token, unique: true
+	end
+
+	def generate_authentication_token!
+		begin
+			self.auth_token = Devise.friendly_token
+		end while self.class.exists?(auth_token: auth_token)
+	end
+
 end
