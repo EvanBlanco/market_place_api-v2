9507851f39c33eff1e0d4c75e8eef49dd9155cea
Evan Blanco <evan.blanco@dotcreek.com>
2018-02-28 09:12:31 -0600

    Updates the total calculation for order

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -29,4 +29,47 @@ describe Order do
 	end
 
 
+	describe "#build_placements_with_product_ids_and_quantities" do
+	end
+
+	describe "#valid?" do
+		before do
+			product_1 = FactoryGirl.create :product, price: 100, quantity: 5
+			product_2 = FactoryGirl.create :product, price: 85, quantity: 10
+
+
+			placement_1 = FactoryGirl.build :placement, product: product_1, quantity: 3
+			placement_2 = FactoryGirl.build :placement, product: product_2, quantity: 15
+
+			@order = FactoryGirl.build :order
+
+			@order.placements << placement_1
+			@order.placements << placement_2
+		end
+
+		it "becomes invalid due to insufficient products" do
+			expect(@order).to_not be_valid
+		end
+	end
+
+
+	describe '#set_total!' do
+    before(:each) do
+      product_1 = FactoryGirl.create :product, price: 100
+      product_2 = FactoryGirl.create :product, price: 85
+
+      placement_1 = FactoryGirl.build :placement, product: product_1, quantity: 3
+      placement_2 = FactoryGirl.build :placement, product: product_2, quantity: 15
+
+      @order = FactoryGirl.build :order
+
+      @order.placements << placement_1
+      @order.placements << placement_2
+    end
+
+    it "returns the total amount to pay for the products" do
+      expect{@order.set_total!}.to change{@order.total.to_f}.from(0).to(1575)
+    end
+  end
+
 end
